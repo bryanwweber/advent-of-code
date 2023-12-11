161ddb604728f5be748afe22319b504e0ceaa166
Bryan Weber <bryan.w.weber@gmail.com>
2023-12-11 18:09:54 -0500

    Complete Day 2 part 2

--- 2023/02/main.go
+++ 2023/02/main.go
@@ -15,12 +15,59 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	lines := strings.Split(string(content), "\n")
+	sumGoodGameIndices(lines)
+	powerOfMinimumCubes(lines)
+}
+
+func powerOfMinimumCubes(lines []string) {
+	powers := make([]int, 0)
+	for _, line := range lines {
+		if line == "" {
+			continue
+		}
+		_, allBatches, found := strings.Cut(line, ":")
+		if !found {
+			log.Fatalf("Couldn't split %s", line)
+		}
+		batches := strings.Split(allBatches, ";")
+		// Copilot says this is a really big integer
+		thisGameMinRed := -1 << 31
+		thisGameMinGreen := -1 << 31
+		thisGameMinBlue := -1 << 31
+		for _, batch := range batches {
+			batch = strings.TrimSpace(batch)
+			allCubes := strings.Split(batch, ",")
+			for _, cube := range allCubes {
+				nCube, color, found := strings.Cut(strings.TrimSpace(cube), " ")
+				if !found {
+					log.Fatalf("Couldn't split cube %s", cube)
+				}
+				nCubeInt, _ := strconv.Atoi(nCube)
+				if color == "red" && nCubeInt > thisGameMinRed {
+					thisGameMinRed = nCubeInt
+				} else if color == "green" && nCubeInt > thisGameMinGreen {
+					thisGameMinGreen = nCubeInt
+				} else if color == "blue" && nCubeInt > thisGameMinBlue {
+					thisGameMinBlue = nCubeInt
+				}
+			}
+		}
+		powers = append(powers, thisGameMinRed*thisGameMinBlue*thisGameMinGreen)
+	}
+	sum := 0
+	for _, pow := range powers {
+		sum += pow
+	}
+	fmt.Println(sum)
+}
+
+func sumGoodGameIndices(lines []string) {
 
 	maxRed := 12
 	maxGreen := 13
 	maxBlue := 14
 	goodGame := make([]int, 0)
-	lines := strings.Split(string(content), "\n")
 	for _, line := range lines {
 		if line == "" {
 			continue
