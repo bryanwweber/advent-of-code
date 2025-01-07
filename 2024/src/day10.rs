f62dc83d6a95c9a94fb7235af337c04711819ee2
Bryan Weber <bryan.w.weber@gmail.com>
2025-01-07 11:35:06 -0500

    Refactor day 10 to a single implementation using iterators

--- 2024/src/day10.rs
+++ 2024/src/day10.rs
@@ -17,193 +17,139 @@ struct Location {
     value: i32,
 }
 
-#[derive(Debug)]
-enum Direction {
-    North,
-    South,
-    East,
-    West,
+#[derive(Copy, Clone)]
+enum TrailCollectionStrategy {
+    Unique,
+    All,
 }
 
-fn find_unique_trail_ends(
-    grid: &Vec<Vec<Location>>,
-    starting_position: Location,
-) -> HashSet<(i32, i32)> {
-    let mut trails: HashSet<(i32, i32)> = HashSet::new();
-    let mut current_positions: VecDeque<Location> = VecDeque::new();
-    current_positions.push_back(starting_position);
-    let max_row = grid.len() - 1;
-    let max_col = grid[0].len() - 1;
-    loop {
-        if current_positions.is_empty() {
-            break;
-        }
-        let current_position = current_positions.pop_front().unwrap();
-        if current_position.value == 9 {
-            trails.insert((current_position.row as i32, current_position.col as i32));
-            continue;
-        }
+trait TrailCollection {
+    fn add(&mut self, point: (i32, i32));
+    fn into_vec(self: Box<Self>) -> Vec<(i32, i32)>;
+}
 
-        let mut neighbors = Vec::new();
-
-        if current_position.col < max_col {
-            neighbors.push((
-                grid[current_position.row][current_position.col + 1],
-                Direction::East,
-            ));
-        }
-        if current_position.col > 0 {
-            neighbors.push((
-                grid[current_position.row][current_position.col - 1],
-                Direction::West,
-            ));
-        }
-        if current_position.row < max_row {
-            neighbors.push((
-                grid[current_position.row + 1][current_position.col],
-                Direction::South,
-            ));
-        }
-        if current_position.row > 0 {
-            neighbors.push((
-                grid[current_position.row - 1][current_position.col],
-                Direction::North,
-            ));
-        }
-        current_positions.extend(
-            neighbors
-                .iter()
-                .filter(|(loc, _)| loc.value == current_position.value + 1)
-                .map(|(loc, _)| *loc)
-                .collect::<Vec<Location>>(),
-        );
+impl TrailCollection for Vec<(i32, i32)> {
+    fn add(&mut self, point: (i32, i32)) {
+        self.push(point);
     }
-    trails
+
+    fn into_vec(self: Box<Self>) -> Vec<(i32, i32)> {
+        *self
+    }
+}
+
+impl TrailCollection for HashSet<(i32, i32)> {
+    fn add(&mut self, point: (i32, i32)) {
+        self.insert(point);
+    }
+
+    fn into_vec(self: Box<Self>) -> Vec<(i32, i32)> {
+        self.into_iter().collect()
+    }
+}
+
+struct Grid {
+    data: Vec<Vec<Location>>,
+}
+
+impl Grid {
+    fn from_lines<I>(lines: I) -> Self
+    where
+        I: Iterator<Item = String>,
+    {
+        let data: Vec<Vec<Location>> = lines
+            .enumerate()
+            .map(|(i_row, line)| {
+                line.chars()
+                    .enumerate()
+                    .map(|(i_col, c)| Location {
+                        row: i_row,
+                        col: i_col,
+                        value: c.to_digit(10).unwrap() as i32,
+                    })
+                    .collect()
+            })
+            .collect();
+        Self { data }
+    }
+
+    fn find_zeros(&self) -> impl Iterator<Item = Location> + '_ {
+        self.data.iter().enumerate().flat_map(|(i_row, row)| {
+            row.iter()
+                .enumerate()
+                .filter(move |(_, loc)| loc.value == 0)
+                .map(move |(i_col, _)| Location {
+                    row: i_row,
+                    col: i_col,
+                    value: 0,
+                })
+        })
+    }
+
+    fn get_neighbors<'a>(&'a self, pos: &'a Location) -> impl Iterator<Item = Location> + 'a {
+        [(0, 1), (0, -1), (1, 0), (-1, 0)]
+            .into_iter()
+            .filter_map(move |(dx, dy)| {
+                let new_row = pos.row as i32 + dx;
+                let new_col = pos.col as i32 + dy;
+
+                if self.is_valid_position(new_row, new_col) {
+                    Some(self.data[new_row as usize][new_col as usize])
+                } else {
+                    None
+                }
+            })
+    }
+
+    fn is_valid_position(&self, row: i32, col: i32) -> bool {
+        row >= 0 && row < self.data.len() as i32 && col >= 0 && col < self.data[0].len() as i32
+    }
+
+    fn find_trails(&self, start: Location, strategy: TrailCollectionStrategy) -> Vec<(i32, i32)> {
+        let mut collection: Box<dyn TrailCollection> = match strategy {
+            TrailCollectionStrategy::Unique => Box::new(HashSet::new()),
+            TrailCollectionStrategy::All => Box::new(Vec::new()),
+        };
+        let mut current_positions: VecDeque<Location> = VecDeque::from([start]);
+        loop {
+            if current_positions.is_empty() {
+                break;
+            }
+            let current_position = current_positions.pop_front().unwrap();
+            if current_position.value == 9 {
+                collection.add((current_position.row as i32, current_position.col as i32));
+                continue;
+            }
+
+            current_positions.extend(
+                self.get_neighbors(&current_position)
+                    .filter(|loc| loc.value == current_position.value + 1),
+            );
+        }
+
+        collection.into_vec()
+    }
+}
+
+fn solve(strategy: TrailCollectionStrategy) {
+    let result = open_file("data/10/input.txt").and_then(|reader| {
+        let grid = Grid::from_lines(reader.lines().filter_map(|line| line.ok()));
+        let trail_count = grid
+            .find_zeros()
+            .map(|loc| grid.find_trails(loc, strategy).len())
+            .sum::<usize>();
+        Ok(trail_count)
+    });
+    match result {
+        Ok(trail_count) => println!("{:?}", trail_count),
+        Err(e) => eprintln!("Error: {}", e),
+    }
 }
 
 pub fn solve_part1() {
-    match open_file("data/10/input.txt") {
-        Ok(reader) => {
-            let lines: Vec<String> = reader.lines().filter_map(|line| line.ok()).collect();
-            let grid: Vec<Vec<Location>> = lines
-                .iter()
-                .enumerate()
-                .map(|(i_row, line)| {
-                    line.chars()
-                        .enumerate()
-                        .map(|(i_col, c)| Location {
-                            row: i_row,
-                            col: i_col,
-                            value: c.to_digit(10).unwrap() as i32,
-                        })
-                        .collect()
-                })
-                .collect();
-            let zeros = grid.iter().enumerate().flat_map(|(i_row, row)| {
-                row.iter()
-                    .enumerate()
-                    .filter(|(_, loc)| loc.value == 0)
-                    .map(move |(i_col, _)| Location {
-                        row: i_row,
-                        col: i_col,
-                        value: 0,
-                    })
-            });
-            let trails = zeros
-                .map(|loc| find_unique_trail_ends(&grid, loc).into_iter().count())
-                .sum::<usize>();
-            println!("{:?}", trails);
-        }
-        Err(e) => eprintln!("Error: {}", e),
-    }
-}
-
-fn find_all_trails(grid: &Vec<Vec<Location>>, starting_position: Location) -> Vec<(i32, i32)> {
-    let mut trails: Vec<(i32, i32)> = Vec::new();
-    let mut current_positions: VecDeque<Location> = VecDeque::new();
-    current_positions.push_back(starting_position);
-    let max_row = grid.len() - 1;
-    let max_col = grid[0].len() - 1;
-    loop {
-        if current_positions.is_empty() {
-            break;
-        }
-        let current_position = current_positions.pop_front().unwrap();
-        if current_position.value == 9 {
-            trails.push((current_position.row as i32, current_position.col as i32));
-            continue;
-        }
-
-        let mut neighbors = Vec::new();
-
-        if current_position.col < max_col {
-            neighbors.push((
-                grid[current_position.row][current_position.col + 1],
-                Direction::East,
-            ));
-        }
-        if current_position.col > 0 {
-            neighbors.push((
-                grid[current_position.row][current_position.col - 1],
-                Direction::West,
-            ));
-        }
-        if current_position.row < max_row {
-            neighbors.push((
-                grid[current_position.row + 1][current_position.col],
-                Direction::South,
-            ));
-        }
-        if current_position.row > 0 {
-            neighbors.push((
-                grid[current_position.row - 1][current_position.col],
-                Direction::North,
-            ));
-        }
-        current_positions.extend(
-            neighbors
-                .iter()
-                .filter(|(loc, _)| loc.value == current_position.value + 1)
-                .map(|(loc, _)| *loc)
-                .collect::<Vec<Location>>(),
-        );
-    }
-    trails
+    solve(TrailCollectionStrategy::Unique);
 }
 
 pub fn solve_part2() {
-    match open_file("data/10/input.txt") {
-        Ok(reader) => {
-            let lines: Vec<String> = reader.lines().filter_map(|line| line.ok()).collect();
-            let grid: Vec<Vec<Location>> = lines
-                .iter()
-                .enumerate()
-                .map(|(i_row, line)| {
-                    line.chars()
-                        .enumerate()
-                        .map(|(i_col, c)| Location {
-                            row: i_row,
-                            col: i_col,
-                            value: c.to_digit(10).unwrap() as i32,
-                        })
-                        .collect()
-                })
-                .collect();
-            let zeros = grid.iter().enumerate().flat_map(|(i_row, row)| {
-                row.iter()
-                    .enumerate()
-                    .filter(|(_, loc)| loc.value == 0)
-                    .map(move |(i_col, _)| Location {
-                        row: i_row,
-                        col: i_col,
-                        value: 0,
-                    })
-            });
-            let trails = zeros
-                .map(|loc| find_all_trails(&grid, loc).into_iter().count())
-                .sum::<usize>();
-            println!("{:?}", trails);
-        }
-        Err(e) => eprintln!("Error: {}", e),
-    }
+    solve(TrailCollectionStrategy::All);
 }
